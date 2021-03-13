3d49e4581fa0e91335721ed5b023cd929d074fb3
Jayromofo <jayromofo@outlook.com>
2021-03-12 23:54:07 -0500

    Made date class and todo list

--- src/Date.java
+++ src/Date.java
@@ -5,6 +5,42 @@
  * * @Email: rossetti.jason@gmail.com
  **/
 
-// TODO: Copy the class from fig 8.7 and then enchance the class from exercise 8.8
+    // TODO: Copy the class from fig 8.7 and then enchance the class from exercise 8.8
+    // TODO: Validate for year
+
 public class Date {
+    private int month;
+    private int day;
+    private int year;
+
+    private static final int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public Date(int month, int day, int year) {
+        if (month <= 0 || month > 12) {
+            throw new IllegalArgumentException("Month  ("+ month + ") must be 1 - 12");
+        }
+        if (day <= 0 || (day > daysPerMonth[month] && !(month == 2 && day == 29))) {
+            throw new IllegalArgumentException("day (" + day + ") out-of-range for the specific month");
+        }
+        if (month == 2 && day == 29 && !(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+            throw new IllegalArgumentException("day (" + day + ") out-of-range for the specific month");
+        }
+        this.month = month;
+        this.day = day;
+        this.year = year;
+
+        System.out.printf("Date object constructor for date %s%n", this);
+    }
+
+    public String toString() {
+        return String.format("%d/%d/%d", month, day, year);
+    }
+
+    // Increments the day by one
+    public void nextDay() {
+
+    }
+
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@
  * * @Email: rossetti.jason@gmail.com
  **/
 
+
+/* TODO: Create a test that calls next day which increments into the next month and then the next year and print
+         it all out.
+         Modify increment Hour to call next day if the time is incremented into the next day.
+         Modify toString and universalToString to output the date as well
+         *** Specifically testing the time to lead into the next day
+
+ */
 public class Main {
 
 
